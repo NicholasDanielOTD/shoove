07fd22d41470eef9053b5b3671237629aac8860a
NicholasDanielOTD <nicholasdaniel97@hotmail.com>
2019-07-18 17:03:29 -0500

    Update shoove.py

--- shoove.py
+++ shoove.py
@@ -10,7 +10,6 @@ import mouse
 IMAGES_PATH = "None"
 PICTURE_HOTKEY = "None"
 CLOSE_HOTKEY = "None"
-x1=x2=y1=y2=None
 
 #Check for config
 config = configparser.ConfigParser()
@@ -23,7 +22,7 @@ else:
     pathin = input("Please enter the path you wish to save images to: ")
     while not os.path.exists(pathin):
         pathin = input("Sorry that directory does not exist, please enter an existing directory: ")
-    config['KEYBINDS'] = {'PICTURE_HOTKEY': 'ctrl+shift+4', 'CLOSE_HOTKEY': 'ctrl+shift+escape'}
+    config['KEYBINDS'] = {'PICTURE_HOTKEY': 'ctrl+shift+4', 'CLOSE_HOTKEY': 'ctrl+5'}
     config['PATH'] = {'IMAGES_PATH':pathin}
     with open('cfg.ini', 'w') as configfile:
         config.write(configfile)
@@ -36,43 +35,54 @@ CLOSE_HOTKEY = config['KEYBINDS']['CLOSE_HOTKEY']
 
 print("Ready to take screenshots!")
 
-def takeScreenshot():
-    print("Taking a screenshot")
-    keyboard.remove_hotkey(takeScreenshot)
-    mouse.on_button(print, args=['yes'], buttons=('left'), types=('down'))
-    mouse.on_button(setFirstCursorPosition, buttons=('left'), types=('down'))
-    keyboard.on_press_key('escape', cancelScreenshot)
+class Screenshot:
 
+	def __init__(self):
+		self.x1=self.y1=self.x2=self.y2=None
+	
+	def takeScreenshot(self):
+		print("Taking a screenshot")
+		keyboard.remove_hotkey(s.takeScreenshot)
+		mouse.on_button(self.setFirstCursorPosition, buttons=('left'), types=('down'))
+		mouse.on_button(self.setSecondCursorPosition, buttons=('left'), types=('up'))
+		keyboard.add_hotkey('escape', s.cancelScreenshot)
 
+	def setFirstCursorPosition(self):
+		print("Setting first position")
+		self.x1 = pyautogui.position()[0]
+		self.y1 = pyautogui.position()[1]
 
-def setFirstCursorPosition():
-    print("Setting first position")
-    x1 = pyautogui.position()[0]
-    y1 = pyautogui.position()[1]
-
-def setSecondCursorPosition():
-    print("Setting second position")
-    x2, y2 = pyautogui.position()
-    mouse.unhook_all()
-    keyboard.remove_hotkey(cancelScreenshot)
-
-    print(x1,y1)
+	def setSecondCursorPosition(self):
+		print("Setting second position")
+		self.x2 = pyautogui.position()[0]
+		self.y2 = pyautogui.position()[1]
+		mouse.unhook_all()
+		keyboard.remove_hotkey('escape')
+		keyboard.add_hotkey(PICTURE_HOTKEY, s.takeScreenshot)
+		
+		self.captureScreen()
+		
+	def cancelScreenshot(self):
+		print("Cancelling screenshot")
+		mouse.unhook_all()
+		keyboard.add_hotkey(PICTURE_HOTKEY, s.takeScreenshot)
+		keyboard.remove_hotkey(s.cancelScreenshot)
     
-def cancelScreenshot():
-    print("Cancelling screenshot")
-    x1=x2=y1=y2=1
-    
+	def captureScreen(self):
+		print(self.x1,self.y1,self.x2,self.y2)
+	
+	
 def shutdown():
-
-    keyboard.unhook_all_hotkeys()
-    exit()
+	keyboard.unhook_all_hotkeys()
+	exit()
     
     
-
-
-keyboard.add_hotkey(PICTURE_HOTKEY, takeScreenshot)
+s = Screenshot()
+	
+keyboard.add_hotkey(PICTURE_HOTKEY, s.takeScreenshot)
 keyboard.add_hotkey(CLOSE_HOTKEY, shutdown)
 
 
 input()
+
 
